0bf6ea19df4bd56ede874177aca8c9521c1b48a5
azsi-exe <azsi@kent.ac.uk>
2026-03-25 18:08:15 +0000

    Test for update mileage has been created

--- MotoLog/test.py
+++ MotoLog/test.py
@@ -8,3 +8,15 @@ def test_add_bike():
     bikes = view_all_bikes()
 
     assert bikes[-1][0] == "TEST01" # Check that the last bike added has the correct registration
+
+# TEST No.2
+def test_update_mileage():
+    add_bike("TEST02", "Honda", "CBR", "500")
+
+    updated = update_mileage("TEST02", "800") # Update mileage
+
+    bikes = view_all_bikes()
+
+    assert updated == True # Checks if the update was successful
+
+    assert bikes[-1][3] == "800" # Check the mileage was actually changed
