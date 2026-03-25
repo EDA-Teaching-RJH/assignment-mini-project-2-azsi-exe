55978e33c2ebb23174dc0ce3359fb2fc0abb0052
azsi-exe <azsi@kent.ac.uk>
2026-03-25 18:16:22 +0000

    Fixed add bike test to check for the existence, rather the last row. test: 1 failed, 2 passed

--- MotoLog/test_data_manager.py
+++ MotoLog/test_data_manager.py
@@ -3,11 +3,17 @@ from data_manager import add_bike, view_all_bikes, update_mileage, delete_bike
 
 # TEST No.1
 def test_add_bike():
-    add_bike("TEST01", "Yamaha", "R125", "1000") # Test bike
+    add_bike("TEST01", "Yamaha", "R125", "1000")
 
     bikes = view_all_bikes()
 
-    assert bikes[-1][0] == "TEST01" # Check that the last bike added has the correct registration
+    found = False # Check if TEST01 exists in the file
+
+    for bike in bikes:
+        if bike[0] == "TEST01":
+            found = True
+
+    assert found == True
 
 # TEST No.2
 def test_update_mileage():
